af38fe8267c66269bbbd82faa811427794ac9d2d
OmarTarekAbbass <omer.tarek@ivas.com.eg>
2020-01-06 09:32:03 +0200

    edit view error page

--- resources/views/front/error.blade.php
+++ resources/views/front/error.blade.php
@@ -1,15 +1,27 @@
 @extends('front.master')
 @section('content')
 <style>
-    .error{
+.error {
+    text-align: center;
+}
+
+.error h2 {
+    color: #f3e5b8;
+}
+
+.error p {
+    color: #f3e5b8;
+}
+
+header {
+    margin-top: -8%;
+}
+
+@media only screen and (min-width: 0) and (max-width: 600px) {
+    .error {
         text-align: center;
         margin-top: 36%;
     }
-.error h2{
-    color: #f3e5b8;
-}
-.error p{
-    color: #f3e5b8;
 }
 </style>
 
@@ -22,7 +34,8 @@
             <p>للعودة الى الصفحة الرئيسية اضغط هنا</p>
 
             <div class="btns">
-                <a class="btn wow pulse" href="{{url('index')}}" data-wow-delay="300ms" data-wow-iteration="infinite" data-wow-duration="1.5s">الصفحة الرئيسية</a>
+                <a class="btn wow pulse" href="{{url('index')}}" data-wow-delay="300ms" data-wow-iteration="infinite"
+                    data-wow-duration="1.5s">الصفحة الرئيسية</a>
             </div>
         </div>
 
@@ -37,10 +50,8 @@ $(function() {
     // url == (this.href)
     url2 = url.split('/')
     console.log(url);
-    $('#tit').html($('#tit').html()+' '+url2[url2.length - 1])
+    $('#tit').html($('#tit').html() + ' ' + url2[url2.length - 1])
 });
 </script>
 @endsection
 /* End Active Menu */
-
-
--- storage/framework/views/28307b28d4e31ceeefcd07b2eea956a72053e8cf.php
+++ storage/framework/views/28307b28d4e31ceeefcd07b2eea956a72053e8cf.php
@@ -1,14 +1,26 @@
 <?php $__env->startSection('content'); ?>
 <style>
-    .error{
+.error {
+    text-align: center;
+}
+
+.error h2 {
+    color: #f3e5b8;
+}
+
+.error p {
+    color: #f3e5b8;
+}
+
+header {
+    margin-top: -8%;
+}
+
+@media  only screen and (min-width: 0) and (max-width: 600px) {
+    .error {
         text-align: center;
         margin-top: 36%;
     }
-.error h2{
-    color: #f3e5b8;
-}
-.error p{
-    color: #f3e5b8;
 }
 </style>
 
@@ -21,7 +33,8 @@
             <p>للعودة الى الصفحة الرئيسية اضغط هنا</p>
 
             <div class="btns">
-                <a class="btn wow pulse" href="<?php echo e(url('index')); ?>" data-wow-delay="300ms" data-wow-iteration="infinite" data-wow-duration="1.5s">الصفحة الرئيسية</a>
+                <a class="btn wow pulse" href="<?php echo e(url('index')); ?>" data-wow-delay="300ms" data-wow-iteration="infinite"
+                    data-wow-duration="1.5s">الصفحة الرئيسية</a>
             </div>
         </div>
 
@@ -36,12 +49,10 @@ $(function() {
     // url == (this.href)
     url2 = url.split('/')
     console.log(url);
-    $('#tit').html($('#tit').html()+' '+url2[url2.length - 1])
+    $('#tit').html($('#tit').html() + ' ' + url2[url2.length - 1])
 });
 </script>
 <?php $__env->stopSection(); ?>
 /* End Active Menu */
-
-
 
 <?php echo $__env->make('front.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ivas_template_laravel5.8\resources\views/front/error.blade.php ENDPATH**/ ?>
